33466c4db22c65c89cb2e84cd3c7e4cd727ba8f9
coderRayhan <rakibrayhan8@gmail.com>
2024-06-23 11:02:54 +0600

    Web project removed

--- src/EmployeeManagement.Web/Interfaces/IEmployeeService.cs
+++
@@ -1,8 +0,0 @@
-using EmployeeManagement.Domain.Entities;
-
-namespace EmployeeManagement.Web.Interfaces;
-public interface IEmployeeService
-{
-    Task<IEnumerable<Employee>> GetEmployeesAsync();
-    Task<Employee> GetEmployeeByIdAsync(int id);
-}
